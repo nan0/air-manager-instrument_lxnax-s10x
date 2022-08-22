f5b251a76f56ee79e99d18b08dac4649b7a849fd
Nans Plancher <nans.plancher@gmail.com>
2022-08-22 08:44:22 +0200

    wind arrow drawing done and implemented, altitude + vario display implemented

--- logic.lua
+++ logic.lua
@@ -1,16 +1,25 @@
 -- Constants
 DEFAULT_FONT = "roboto_regular.ttf"
 DEFAULT_COLOR = "white"
+DEFAULT_LABEL_SIZE = 30
 
--- Initialisations --
+-- Background
 img_add_fullscreen("ls100_bg.png")
+
+-- Altitude
+txt_add("Altitude", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:" .. DEFAULT_LABEL_SIZE .. "; valign:center; halign: right;", 175, 150, 155, DEFAULT_LABEL_SIZE)
+txt_add("ft", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:" .. DEFAULT_LABEL_SIZE .. "; valign:center; halign: right;", 175, 205, 155, DEFAULT_LABEL_SIZE)
+textbox_altitude = txt_add("0", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:80; valign:center; halign: right;", 160, 165, 155, 80)
+
+-- Vario
+txt_add("Variometer", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:" .. DEFAULT_LABEL_SIZE .. "; valign:center; halign: right;", 175, 265, 150, DEFAULT_LABEL_SIZE)
+txt_add("m/s", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:" .. DEFAULT_LABEL_SIZE .. "; valign:center; halign: right;", 175, 320, 155, DEFAULT_LABEL_SIZE)
+textbox_vario = txt_add("0", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:80; valign:center; halign: right;", 135, 280, 155, 80)
 img_vario_needle = img_add("ls100_vario_needle.png", 0, 0, 512, 512) -- TODO: fetch canvas size from settings
--- img_maccready_needle = img_add("vario_neddle_VN.png", 0, 0, 512, 512) -- TODO: fetch canvas size from settings
 
-textbox_altitude = txt_add("0", " font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";  size:80; valign:center; halign: right;", 140, 320, 155, 50)
-
-img_wind_arrow = img_add("arrow_sm.png", 130, 245, 24, 33)
-textbox_wind_dir = txt_add("0°", "font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. "; size:32; valign:center; halign: right;", 100, 280, 55, 35)
+-- Wind
+img_wind_arrow = img_add("ls100_wind_arrow.png", 130, 222.5, 35, 35)
+textbox_wind_dir = txt_add("0°", "font:" .. DEFAULT_FONT .. "; color:" .. DEFAULT_COLOR .. ";size:" .. DEFAULT_LABEL_SIZE .. "; valign:center; halign: center;", 122.5, 265, 55, DEFAULT_LABEL_SIZE)
 
 -- Functions --
 function display_wind_direction(direction)
@@ -24,28 +33,28 @@ function display_wind_direction(direction)
 end
 
 function display_ground_altitude(altitude)
-    ft_altitude = altitude * 328084 / 100000
+    local ft_altitude = altitude * 328084 / 100000
     ft_altitude = var_cap(ft_altitude, 0, 9999)
     ft_altitude = var_format(ft_altitude, 0)
     txt_set(textbox_altitude, ft_altitude)
 end
 
---[[ function dislay_vs(vs)
-    vs = var_cap(vs, -1300, 1300)
-    rotate(img_vario_needle, 180.0 / 2000 * vs)
-end
-]]--
-
 function dislay_vario(vn)
-    vn = var_cap(vn, -10.0, 10.0)
-    rotate(img_vario_needle, 240 / 20.0 * vn)
+    local needle_vn = var_cap(vn, -5.0, 5.0)
+    rotate(img_vario_needle, 240 / 10 * needle_vn)
+
+    vn = var_round(vn, 1)
+    local op = ""
+    if vn > 0 then
+        op = "+"
+    end
+    local formated_str = string.format("%s%.1f", op, vn)
+    txt_set(textbox_vario, formated_str)
 end
 
 -- Subscriptions
 fsx_variable_subscribe("AMBIENT WIND DIRECTION", "Degrees", display_wind_direction)
 
 fsx_variable_subscribe("GROUND ALTITUDE", "Meters", display_ground_altitude)
-
--- fsx_variable_subscribe("VERTICAL SPEED", "Feet per minute", dislay_vs)
 
 fs2020_variable_subscribe("L:VARIO_NEEDLE", "FLOAT", dislay_vario)
